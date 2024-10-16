4542cb9f3eade60b33b4692a54a2878032e20d43
AlfredMoller <almoller622@gmail.com>
2024-10-16 14:29:05 -0300

    Se modifica logica de validacion

--- app/Controllers/Http/ProductController.ts
+++ app/Controllers/Http/ProductController.ts
@@ -41,26 +41,18 @@ export default class ProductController {
       })
 
      // Validar que 'desde' y 'total' no sean 0
-    if (parametros.desde <= 0 || parametros.total <= 0) {
+    // Validar que 'total' no sea menor que 'desde' y que 'total' no sea 0
+    if (parametros.total <= 0 || parametros.total < parametros.desde) {      
       return response.status(400).send({
         status: 'ERROR',
-        mensaje: "Los parámetros 'desde' y 'total' deben ser mayores a 0",
+        mensaje: parametros.total <= 0 
+          ? "El valor de 'total' debe ser mayor que 0" 
+          : "El valor de 'total' no puede ser menor que 'desde'",
         code: 400,
         _idSolicitud: parametros._idSolicitud,
         data: null,
       });
     }
-
-    // validar que el Total no sea menor al Desde
-    if (parametros.total < parametros.desde) {      
-      return response.status(400).send({
-        status: 'ERROR',
-        mensaje: "El valor de 'total' no puede ser menor que 'desde'",
-        code: 400,
-        _idSolicitud: parametros._idSolicitud,
-        data: null,
-      });
-    } 
 
       // Se arma estructura de query similar a stringbuilder 
 
